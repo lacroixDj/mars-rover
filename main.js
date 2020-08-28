9437f74b9668f329235bdbfe47b2e7eadb526207
J. Frank Lacroix <lacroixdj@gmail.com>
2020-08-28 23:58:40 +0100

    commiting all the local work so far

---
+++ main.js
@@ -0,0 +1,27 @@
+// Custom lib for handling input options / vavlues
+const input = require('./lib/services/input');
+
+// Custom lib for handling / print app ouput and errors
+const output = require('./lib/services/output');
+
+// Custom lib for handling app robots logic
+//const process = require('./lib/process');
+
+/**
+ * Starting point of the javascript program, 
+ * it has the main application logic flow by invoking 
+ * utility classes located in  src/ 
+ * 
+ * @author Frank Lacroix <lacroixdj@gmail.com.com>
+ */
+const main = () => {
+    
+    input.readInput()
+        
+        .then( robots_output => output.printOutput(robots_output))
+        
+        .catch(error => output.printError(error));
+}
+
+// Exporting the class instance
+module.exports = main;
