32e64b8df3e6e801864b3aeb2bf28a48d862c50b
J. Frank Lacroix <lacroixdj@gmail.com>
2020-08-31 12:51:50 +0100

    Daemonizing Node cli app  inside Docker container

--- build.sh
+++ build.sh
@@ -17,18 +17,18 @@ echo
 
 echo "--> Running the container in detached mode:  docker run --name martian-robots -d lacroixdj/martian-robots:latest"
 docker run --name martian-robots --restart always lacroixdj/martian-robots:latest
-echo
+# echo
 
-echo "--> Runing tests: docker exec martian-robots npm test "
-docker exec -d martian-robots npm test
-echo
+# echo "--> Runing tests: docker exec martian-robots npm test "
+# docker exec -d martian-robots npm test
+# echo
 
-echo "--> Inspecting logs output: docker logs martian-robots "
-docker logs martian-robots
-echo
+# echo "--> Inspecting logs output: docker logs martian-robots "
+# docker logs martian-robots
+# echo
 
-echo "--> Verifying the container is actually running: docker ps | grep martian-robots "
-docker ps | grep martian-robots
-echo
+# echo "--> Verifying the container is actually running: docker ps | grep martian-robots "
+# docker ps | grep martian-robots
+#echo
 
-echo "--> /End."
+# echo "--> /End."
